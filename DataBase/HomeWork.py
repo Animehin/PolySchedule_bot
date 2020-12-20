682f73a26074fde4c8b740867bdb61c2e7308421
y.stashevskiy <y.stashevskiy@okko.tv>
2020-12-20 21:22:39 +0300

    -Reworked read_home_work

--- DataBase/HomeWork.py
+++ DataBase/HomeWork.py
@@ -1,4 +1,5 @@
 import re
+from datetime import datetime
 
 import pymongo
 
@@ -12,7 +13,7 @@ def upd_home_work(pgroup, date, className, homeWork):
                             {"$set": {"homeWork": homeWork}})
 
 
-def add_home_work(pgroup, date, className, homeWork, tgLogin):
+def add_home_work(pgroup, date, time, className, homeWork, tgLogin):
     if (re.search('[0-9]', pgroup) is None) or (re.search('[а-яА-Я]', className) is None) or (
             re.search('[а-яА-Я]', homeWork) is None) or (re.search('[a-zA-Z]', tgLogin) is None):
         return False
@@ -21,7 +22,8 @@ def add_home_work(pgroup, date, className, homeWork, tgLogin):
             {"pgroup": pgroup, "date": date, "className": className, "tgLogin": tgLogin}) is None:
         try:
             collectionHW.insert_one({
-                "pgroup": pgroup, "date": date, "className": className, "homeWork": homeWork, "tgLogin": tgLogin
+                "pgroup": pgroup, "date": date, "time": time, "className": className, "homeWork": homeWork,
+                "tgLogin": tgLogin
             })
         except pymongo.errors.WriteError:
             return False
@@ -31,11 +33,15 @@ def add_home_work(pgroup, date, className, homeWork, tgLogin):
     return True
 
 
-def read_home_work(pgroup, date, className):
+def read_home_work(pgroup):
+    today = datetime.date.today().format("%d.%m.%Y")
     hwlist = []
-    res = collectionHW.find({"pgroup": pgroup, "date": date, "className": className}, {"_id": 0})
+    res = collectionHW.find({"pgroup": pgroup}, {"_id": 0})
     if res is None:
         return hwlist
-    for i in res:
-        hwlist.append(i)
+    for lesson in res:
+        if lesson["date"].format("%d.%m.%Y") >= today:
+            hwlist.append(lesson)
+        else:
+            collectionHW.delete_one({"date": lesson["date"]})
     return hwlist
