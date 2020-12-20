4bcb2d9b712db88aeb6a537c7600f0436e90d335
y.stashevskiy <y.stashevskiy@okko.tv>
2020-12-20 19:26:30 +0300

    +Reworked upd_schedule function

--- DataBase/Schedule.py
+++ DataBase/Schedule.py
@@ -1,25 +1,41 @@
+import datetime
+
 import pymongo
+
+from PolySchedule_bot.ScheduleProvider import ScheduleClient
 
 global last_updated
 clientSch = pymongo.MongoClient('localhost', 27017)
 dbSch = clientSch['TgBot']
 collectionSch = dbSch['TgBotSchedule']
+last_updated = {}
 
 
-def upd_schedule(pgroup, o_date, o_time, n_date, n_time, scheduledClass):
-    collectionSch.update_one({"pgroup": pgroup, "date": o_date, "time": o_time, "scheduledClass": scheduledClass},
-                             {"$set": {"date": n_date, "time": n_time}})
-
-
-def add_schedule(pgroup, time, date, ClassName):
-    collectionSch.insert_one({"pgroup": pgroup, "time": time, "date": date, "scheduledClass": ClassName})
-
-
-def read_schedule(pgroup, date=None, time=None):
+def upd_schedule(pgroup):
     global last_updated
-    if last_updated is None:
-        raise Exception("Расписание не обновлялось")
-    if date and time is None:
+
+    today = datetime.date.today().strftime("%d.%m.%Y")
+    schVar = ScheduleClient.getSchedule(pgroup, today)
+    if schVar is "":
+        return "Фейлед ту апдэйте"
+    collectionSch.delete_many({"pgroup": pgroup})
+
+    for day in schVar:
+        for lesson in schVar[day]:
+            collectionSch.insert_one(
+                {"pgroup": pgroup, "date": day, "time": lesson["time"], "scheduledClass": lesson["scheduledClass"],
+                 "classType": lesson["classType"]})
+    last_updated[pgroup] = today
+    return "Апдейт кумплитэд"
+
+
+def read_schedule(pgroup, date=None):
+    global last_updated
+
+    if (last_updated is None) or (last_updated is not datetime.date.today().strftime("%d.%m.%Y")):
+        upd_schedule(pgroup)
+
+    if date is None:
         schVar = collectionSch.find_one({"pgroup": pgroup}, {"_id": 0})
     else:
         schVar = collectionSch.find_one({"date": date, "pgroup": pgroup}, {"_id": 0})
@@ -28,7 +44,6 @@ def read_schedule(pgroup, date=None, time=None):
             check_schedule_collection_availability()
         except Exception:
             return "Ошибка {}".format(Exception)
-    last_updated = date
     return schVar
 
 
