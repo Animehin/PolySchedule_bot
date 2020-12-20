82a08d1d0d4567a37f32e31fa2beea5a867fad29
y.stashevskiy <y.stashevskiy@okko.tv>
2020-12-20 23:05:32 +0300

    -Removed unused entity

--- DataBase/HomeWork.py
+++ DataBase/HomeWork.py
@@ -2,19 +2,14 @@ import re
 from datetime import datetime
 
 import pymongo
-import pytz
 from dateutil.parser import parse
 
-utc = pytz.UTC
+# import pytz
+# utc = pytz.UTC
 
 clientHW = pymongo.MongoClient('localhost', 27017)
 dbHW = clientHW['TgBot']
 collectionHW = dbHW['TgBotHW']
-
-
-def upd_home_work(pgroup, date, className, homeWork):
-    collectionHW.update_one({"pgroup": pgroup, "date": date, "className": className},
-                            {"$set": {"homeWork": homeWork}})
 
 
 def add_home_work(pgroup, date, time, className, classType, homeWork, tgLogin):
@@ -23,23 +18,28 @@ def add_home_work(pgroup, date, time, className, classType, homeWork, tgLogin):
         return False
 
     if collectionHW.find_one(
-            {"pgroup": pgroup, "date": date, "className": className, "classType": classType,
+            {"pgroup": pgroup, "date": date, "time": time, "className": className, "classType": classType,
              "tgLogin": tgLogin}) is None:
         try:
             collectionHW.insert_one({
-                "pgroup": pgroup, "date": date, "time": time, "className": className, "homeWork": homeWork,
+                "pgroup": pgroup, "date": date, "time": time, "className": className, "classType": classType,
+                "homeWork": homeWork,
                 "tgLogin": tgLogin
             })
         except pymongo.errors.WriteError:
             return False
     else:
-        upd_home_work(pgroup, date, className, homeWork)
+        collectionHW.update_one(
+            {"pgroup": pgroup, "date": date, "time": time, "className": className, "classType": classType,
+             "tgLogin": tgLogin},
+            {"$set": {"homeWork": homeWork}})
 
     return True
 
 
 def read_home_work(pgroup):
-    today = utc.localize(datetime.today())
+    # today = utc.localize(datetime.today())
+    today = datetime.datetime.today()
     hwlist = []
     res = collectionHW.find({"pgroup": pgroup}, {"_id": 0})
     if res is None:
@@ -50,6 +50,3 @@ def read_home_work(pgroup):
         else:
             collectionHW.delete_one({"date": lesson["date"]})
     return hwlist
-
-
-read_home_work("3530904/70105")
